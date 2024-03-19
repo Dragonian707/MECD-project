b7f753877f43230ddf405c6f72fdbe2ce17bb2ab
Dragonian707 <jarnodijk2001@hotmail.com>
2024-03-19 11:41:43 +0100

    Start of wall script and added grass sprite

---
+++ Assets/Scripts/WallManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WallManager : MonoBehaviour
+{
+    [SerializeField] Sprite blankTile;
+    [SerializeField] Sprite grassTile;
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+        //make a field full of blank tiles able to be converted
+        //every sprite will be 30 x 30 to get a grid of 50 x 25 tiles perfectly
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Test()
+    {
+
+    }
+}
